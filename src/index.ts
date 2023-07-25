739eb2fb242f953a4af581f929e57116672e020c
Swarup <suryawanshiswarup@gamil.com>
2023-07-25 15:13:56 +0530

    init graphql

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 // src/index.ts
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 import config from "./config";
 
+// Define the GraphQL schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// Root resolver for the API
+const root = {
+  hello: () => "Hello, GraphQL!",
+};
+
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true, // Enable GraphiQL interface for testing in the browser
+  })
+);
+
 const port = config.port;
-
-app.get("/", (req, res) => {
-  res.send("Hello, Express TypeScript!");
-});
-
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}/graphql`);
 });
